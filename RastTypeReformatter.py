91eaf2d732c04ca090817ac666e43db31f01a4ef
Phil <83251507+Phillip-Ruscica@users.noreply.github.com>
2021-04-29 14:39:11 -0400

    Fixed toolbox and progressor
    Fixed toolbox connection to script. Removed progressor since changing its variables doesn't do anything.

--- RastTypeReformatter.py
+++ RastTypeReformatter.py
@@ -13,19 +13,24 @@ import arcpy
 ###############################################################################
 
 
-def main(parent_folder, output_folder, type):
+def main(parent_folder, output_folder, rast_type):
     # Set the workspace to the raster folder
     arcpy.env.workspace = parent_folder
 
     # Produce a list of all tifs in the parent folder
     rast_list = arcpy.ListRasters()
+    
+    # Get the number of rasters in the directory
     list_len = len(rast_list)
-    arcpy.SetProgressor('step', "Converting raster", 0, list_len, 1)
+    
+    # Create a progressor
+    arcpy.SetProgressor('default')
 
     # Iterate through all rasters and conver them
     for item in rast_list:
-        arcpy.RasterToOtherFormat_conversion(item, output_folder, type)
-        arcpy.SetProgressorPosition()
+        # Convert the raster
+        arcpy.RasterToOtherFormat_conversion(item, output_folder, rast_type)
+        # Update the user to which raster was converted
         arcpy.AddMessage(item)
 
 
@@ -38,7 +43,7 @@ if __name__ == '__main__':
     # Get the directory for raster output
     output_folder = arcpy.GetParameterAsText(1)
     # Get the type of raster to output
-    type = arcpy.GetParameterAsText(2)
+    rast_type = arcpy.GetParameterAsText(2)
 
     # Run the script
-    main(parent_folder, output_folder, type)
+    main(parent_folder, output_folder, rast_type)
